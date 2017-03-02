842ed203b4006be28f20b0b816110b457afa7bf2
Sefa Sia Deniz <sefasaiddeniz1@gmail.com>
2017-03-03 02:34:12 +0300

    Changed get to post method

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var port = process.env.PORT || 3000;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:3000/)
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 // REGISTER OUR ROUTES -------------------------------
